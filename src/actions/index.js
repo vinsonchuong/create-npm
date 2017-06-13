55e2cdfdc2cf6eaf8f77ceef11c3fd12cc23593e
Vinson Chuong <vinsonchuong@gmail.com>
2017-06-13 11:25:10 -0700

    Add action for reading npm auth token

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,4 @@
 export { default as getAuthor } from './getAuthor'
 export { default as getAuthorEmail } from './getAuthorEmail'
 export { default as getAuthorName } from './getAuthorName'
+export { default as getNpmToken } from './getNpmToken'
