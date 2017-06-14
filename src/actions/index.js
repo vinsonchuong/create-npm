7de67cb4437f7d75df18ebfe6cc2af4ff952eff6
Vinson Chuong <vinsonchuong@gmail.com>
2017-06-14 12:20:00 -0700

    Sort actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 /* @flow */
+export { default as createGitHubRepository } from './createGitHubRepository'
 export { default as getAuthor } from './getAuthor'
 export { default as getAuthorEmail } from './getAuthorEmail'
 export { default as getAuthorName } from './getAuthorName'
 export { default as getNpmToken } from './getNpmToken'
-export { default as createGitHubRepository } from './createGitHubRepository'
