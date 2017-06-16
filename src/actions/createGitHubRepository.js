6d58e0e732132787a0a3beeb19ec781a43bece91
Vinson Chuong <vinsonchuong@gmail.com>
2017-06-15 21:38:42 -0700

    Separate responsibilities of createGitHubRepository

--- src/actions/createGitHubRepository.js
+++ src/actions/createGitHubRepository.js
@@ -1,9 +1,7 @@
 /* @flow */
 import { exec } from 'create-npm/src/io'
-import { getGitHubRepositoryName } from 'create-npm/src/actions'
 
-export default async function (localPath: string): Promise<string> {
+export default async function (localPath: string): Promise<void> {
   await exec(`git init '${localPath}'`)
   await exec('hub create', { cwd: localPath })
-  return getGitHubRepositoryName(localPath)
 }
