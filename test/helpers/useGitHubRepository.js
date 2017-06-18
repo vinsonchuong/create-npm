90f78c3dee6c081452ec8242031189eca9c596f2
Vinson Chuong <vinsonchuong@gmail.com>
2017-06-18 12:55:04 -0700

    Add GitHub token when cloning repos

--- test/helpers/useGitHubRepository.js
+++ test/helpers/useGitHubRepository.js
@@ -1,11 +1,14 @@
 /* @flow */
 import test from 'ava'
 import * as path from 'path'
+import u from 'updeep'
 import { exec, removeDirectory } from 'create-npm/src/io'
 
 export default function (repoName: string): string {
   test.beforeEach(async t => {
-    await exec('hub clone test-create-npm/existing-repo')
+    await exec('hub clone test-create-npm/existing-repo', {
+      env: u({ GITHUB_TOKEN: process.env.GITHUB_TEST_TOKEN }, process.env)
+    })
   })
 
   test.afterEach.always(async t => {
