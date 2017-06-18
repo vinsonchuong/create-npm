f956e8525d58983d9908065b4dcf508acd120107
Vinson Chuong <vinsonchuong@gmail.com>
2017-06-18 14:49:56 -0700

    Revert "Use git instead of hub to clone repos"
    This reverts commit 65b815dd856139799a99d94a69b94fcbd7c899d5.

--- test/helpers/useGitHubRepository.js
+++ test/helpers/useGitHubRepository.js
@@ -1,11 +1,14 @@
 /* @flow */
 import test from 'ava'
 import * as path from 'path'
+import u from 'updeep'
 import { exec, removeDirectory } from 'create-npm/src/io'
 
 export default function (slug: string): string {
   test.beforeEach(async t => {
-    await exec(`git clone https://github.com/${slug}`)
+    await exec(`hub clone ${slug}`, {
+      env: u({ GITHUB_TOKEN: process.env.GITHUB_TEST_TOKEN }, process.env)
+    })
   })
 
   test.afterEach.always(async t => {
