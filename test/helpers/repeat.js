42f02c57e0cff750750398c374ccaaac053db7d0
Vinson Chuong <vinsonchuong@gmail.com>
2017-06-25 13:18:32 -0700

    Do not repeat tests

--- test/helpers/repeat.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-export default function repeat (repetitions: number, action: () => void): void {
-  if (repetitions < 1) {
-    return
-  }
-  action()
-  return repeat(repetitions - 1, action)
-}
