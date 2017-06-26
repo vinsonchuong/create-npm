43322eec61a2336d93db58762cd11303bf80c95c
Vinson Chuong <vinsonchuong@gmail.com>
2017-06-25 19:55:25 -0700

    Sync with travis in a more robust way

--- src/actions/enableTravis.js
+++ src/actions/enableTravis.js
@@ -7,12 +7,11 @@ export default async function (slug: string): Promise<void> {
 }
 
 async function sync (): Promise<void> {
-  try {
+  const status = await exec('travis sync --check --no-interactive')
+  if (status === 'not syncing') {
     await exec('travis sync')
-  } catch (error) {
-    if (error.message.includes('Sync already in progress')) {
-      await sleep(1000)
-      return sync()
-    }
+  } else {
+    await sleep(1000)
+    return sync()
   }
 }
