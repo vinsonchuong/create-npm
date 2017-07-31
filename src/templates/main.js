c42e0bf4b575e1aca00a3cabf64c1a46dab90b1a
Vinson Chuong <vinsonchuong@gmail.com>
2017-07-30 19:42:29 -0700

    Import by package main

--- src/templates/main.js
+++ src/templates/main.js
@@ -3,7 +3,7 @@ import type { Template } from './'
 import { file } from './'
 
 export default function(): Template {
-  const path = `src/greeting.js`
+  const path = `src/index.js`
   const content = file`
     /* @flow */
     export default 'Hello World!'
