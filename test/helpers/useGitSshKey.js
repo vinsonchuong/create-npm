08d21f8201cd83ed6ea2582908be0a3338733d71
Vinson Chuong <vinsonchuong@gmail.com>
2018-01-14 16:09:16 -0800

    Update dependencies

--- test/helpers/useGitSshKey.js
+++ test/helpers/useGitSshKey.js
@@ -3,7 +3,7 @@ import test from 'ava'
 import * as path from 'path'
 
 export default function(): void {
-  test.beforeEach(async t => {
+  test.beforeEach(t => {
     process.env.GIT_SSH_COMMAND = `ssh -i ${path.resolve('keys', 'id_rsa')}`
     process.env.SSH_AUTH_SOCK = ''
   })
