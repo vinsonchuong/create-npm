bdb75fe8dba047b9adcc1ce3e7f2b84bf228da79
Vinson Chuong <vinsonchuong@gmail.com>
2018-01-14 21:03:55 -0800

    Fix typo

--- src/actions/encryptForTravis.js
+++ src/actions/encryptForTravis.js
@@ -3,7 +3,7 @@ import { exec } from 'create-npm/src/io'
 
 export default async function(slug: string, data: string): Promise<string> {
   const quotedPayload = await exec(
-    `travis enable --no-interactive --repo '${slug}' '${data}'`
+    `travis encrypt --no-interactive --repo '${slug}' '${data}'`
   )
   return quotedPayload.slice(1, -1)
 }
