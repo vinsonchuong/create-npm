508c874c983ebf7fae74ab8e5953b54b8aaad5cf
Vinson Chuong <vinsonchuong@gmail.com>
2018-08-16 15:30:49 -0700

    refactor(create-npm): Prepare for rewrite

--- src/actions/encryptForTravis.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-import { exec } from 'create-npm/src/io'
-
-export default async function(slug: string, data: string): Promise<string> {
-  const quotedPayload = await exec(
-    `travis encrypt --no-interactive --repo '${slug}' '${data}'`
-  )
-  return quotedPayload.slice(1, -1)
-}
