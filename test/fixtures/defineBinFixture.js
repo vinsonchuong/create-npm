67210909b37fa94ba55e3c56c175d0da177f3c31
Vinson Chuong <vinsonchuong@gmail.com>
2018-08-16 15:30:49 -0700

    refactor(create-npm): Prepare for rewrite

--- test/fixtures/defineBinFixture.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-module.exports = function(outputMap) {
-  const args = process.argv.slice(2).join(' ')
-  if (args in outputMap) {
-    process.stdout.write(`${outputMap[args]}\n`)
-  } else {
-    throw new Error(`Unexpected Args: ${args}`)
-  }
-}
