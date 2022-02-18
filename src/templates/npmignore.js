4541271a41077638a108441f8126d37aa0f624a9
Vinson Chuong <vinsonchuong@gmail.com>
2022-02-17 19:58:07 -0800

    fix(npmignore): Exclude more unneeded files from being packaged

--- src/templates/npmignore.js
+++ src/templates/npmignore.js
@@ -2,14 +2,13 @@ export default function () {
   return {
     path: '.npmignore',
     content: `
+      yarn.lock
+      .gitignore
+      .github/
+
       **/*.test.js
       **/test.js
       test/
-
-      .yarn/
-      .yarnrc.yml
-
-      .github/
     `,
   }
 }
