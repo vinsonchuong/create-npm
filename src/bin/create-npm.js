c4ac3353f2b15d4111b6d724aa9753a95f2ab560
Vinson Chuong <vinsonchuong@gmail.com>
2023-03-19 12:38:18 -0700

    test(xo): Fix linting errors

--- src/bin/create-npm.js
+++ src/bin/create-npm.js
@@ -22,63 +22,56 @@ import {
 } from '../templates/index.js'
 import {addPackages} from '../../lib/yarn/index.js'
 
-async function run() {
-  const repoName = process.argv[2]
-  const githubToken = process.env.GITHUB_TOKEN
-  const npmToken = process.env.NPM_TOKEN
+const repoName = process.argv[2]
+const githubToken = process.env.GITHUB_TOKEN
+const npmToken = process.env.NPM_TOKEN
 
-  console.log('\n')
+console.log('\n')
 
-  if (!repoName || !githubToken || !npmToken) {
-    console.log('Usage: GITHUB_TOKEN=? NPM_TOKEN=? yarn create user/repo')
-    return
-  }
-
-  const [, packageName] = repoName.split('/')
-  const branchName = (await getConfig('init.defaultBranch')) || 'master'
-
-  console.log('Creating GitHub Repository')
-  const githubApi = await authenticate(githubToken)
-  await createRepo(githubApi, repoName)
-
-  console.log('Adding NPM Token to GitHub Repository')
-  await addSecretToRepo(githubApi, repoName, 'NPM_TOKEN', npmToken)
-
-  console.log('Writing project files')
-  const today = new Date()
-  const authorName = await getConfig('user.name')
-  const authorEmail = await getConfig('user.email')
-  const projectDirectory = path.resolve(packageName)
-
-  await writeTemplate(projectDirectory, readme({repoName}))
-  await writeTemplate(projectDirectory, license({authorName, today}))
-  await writeTemplate(projectDirectory, gitignore({}))
-  await writeTemplate(
-    projectDirectory,
-    packagejson({repoName, authorName, authorEmail, branchName}),
-  )
-  await writeTemplate(projectDirectory, npmignore({}))
-  await writeTemplate(projectDirectory, main({}))
-  await writeTemplate(projectDirectory, test({}))
-  await writeTemplate(projectDirectory, ciAction({branchName}))
-  await writeTemplate(projectDirectory, dependabotAction({}))
-  await writeTemplate(projectDirectory, dependabot({}))
-
-  console.log('Installing npm Packages')
-  await addPackages(projectDirectory, 'development', [
-    'ava',
-    'xo',
-    'semantic-release',
-  ])
-
-  console.log('Committing Changes')
-  await commitChanges(
-    projectDirectory,
-    `feat(${packageName}): Bootstrap project`,
-  )
-
-  console.log('Pushing Changes')
-  await pushChanges(projectDirectory, repoName, branchName, githubToken)
+if (!repoName || !githubToken || !npmToken) {
+  console.log('Usage: GITHUB_TOKEN=? NPM_TOKEN=? yarn create user/repo')
+  process.exit(1)
 }
 
-run()
+const [, packageName] = repoName.split('/')
+const branchName = (await getConfig('init.defaultBranch')) || 'master'
+
+console.log('Creating GitHub Repository')
+const githubApi = authenticate(githubToken)
+await createRepo(githubApi, repoName)
+
+console.log('Adding NPM Token to GitHub Repository')
+await addSecretToRepo(githubApi, repoName, 'NPM_TOKEN', npmToken)
+
+console.log('Writing project files')
+const today = new Date()
+const authorName = await getConfig('user.name')
+const authorEmail = await getConfig('user.email')
+const projectDirectory = path.resolve(packageName)
+
+await writeTemplate(projectDirectory, readme({repoName}))
+await writeTemplate(projectDirectory, license({authorName, today}))
+await writeTemplate(projectDirectory, gitignore({}))
+await writeTemplate(
+  projectDirectory,
+  packagejson({repoName, authorName, authorEmail, branchName}),
+)
+await writeTemplate(projectDirectory, npmignore({}))
+await writeTemplate(projectDirectory, main({}))
+await writeTemplate(projectDirectory, test({}))
+await writeTemplate(projectDirectory, ciAction({branchName}))
+await writeTemplate(projectDirectory, dependabotAction({}))
+await writeTemplate(projectDirectory, dependabot({}))
+
+console.log('Installing npm Packages')
+await addPackages(projectDirectory, 'development', [
+  'ava',
+  'xo',
+  'semantic-release',
+])
+
+console.log('Committing Changes')
+await commitChanges(projectDirectory, `feat(${packageName}): Bootstrap project`)
+
+console.log('Pushing Changes')
+await pushChanges(projectDirectory, repoName, branchName, githubToken)
