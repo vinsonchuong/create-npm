0d804c3573c1f2d470d52dcae45484481cab3f7e
Vinson Chuong <vinsonchuong@gmail.com>
2023-03-20 10:36:38 -0700

    fix(readme): Remove dependency badges. The service powering them no longer works

--- src/templates/readme.js
+++ src/templates/readme.js
@@ -7,8 +7,6 @@ export default function ({repoName}) {
       # ${packageName}
       [![npm](https://img.shields.io/npm/v/${packageName}.svg)](https://www.npmjs.com/package/${packageName})
       [![CI Status](https://github.com/${repoName}/workflows/CI/badge.svg)](https://github.com/${repoName}/actions?query=workflow%3ACI)
-      [![dependencies Status](https://david-dm.org/${repoName}/status.svg)](https://david-dm.org/${repoName})
-      [![devDependencies Status](https://david-dm.org/${repoName}/dev-status.svg)](https://david-dm.org/${repoName}?type=dev)
 
       An awesome package
 
